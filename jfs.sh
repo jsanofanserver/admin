e190041570ec9ba1e29991c1a6c8c2de8b80fc7e
Chris Kreidler <ckreidler@gmail.com>
2016-08-02 21:18:54 -0700

    Change all instances to named tmux session

--- jfs.sh
+++ jfs.sh
@@ -30,13 +30,13 @@ save() {
 # keeps monthly backups forever
 backup() {
     if [ $1 == "bihourly" ]; then
-        tmux -q send -t 0 "save-all" C-m
+        tmux -q send -t vanilla "save-all" C-m
         sleep 1
-        tmux -q send -t 0 "save-off" C-m
+        tmux -q send -t vanilla "save-off" C-m
         FILENAME="vanilla-backup_$(date +%F_%H-%M)_bihourly.tar.gz"
         cd /home/jfs/vanilla/
         tar czf /home/jfs/vanilla/backup/$FILENAME world
-        tmux -q send -t 0 "save-on" C-m
+        tmux -q send -t vanilla "save-on" C-m
         cd /home/jfs/vanilla/backup
         /bin/ls -t | grep "bihourly" | awk 'NR>22' | xargs rm
         #lftp -c "open -u ********,******** dedibackup-dc2.online.net; mirror -Re . /jfs"
@@ -44,32 +44,32 @@ backup() {
         d=$(date +%d)
         u=$(date +%u)
         if [ $u -eq "1" ] && [ $d -lt "8" ]; then
-            tmux -q send -t 0 "save-all" C-m
+            tmux -q send -t vanilla "save-all" C-m
             sleep 1
-            tmux -q send -t 0 "save-off" C-m
+            tmux -q send -t vanilla "save-off" C-m
             FILENAME="vanilla-backup_$(date +%F_%H-%M)_monthly.tar.gz"
             cd /home/jfs/vanilla/
             tar czf /home/jfs/vanilla/backup/$FILENAME world
-            tmux -q send -t 0 "save-on" C-m
+            tmux -q send -t vanilla "save-on" C-m
         elif [ $u -eq "1" ]; then
-            tmux -q send -t 0 "save-all" C-m
+            tmux -q send -t vanilla "save-all" C-m
             sleep 1
-            tmux -q send -t 0 "save-off" C-m
+            tmux -q send -t vanilla "save-off" C-m
             FILENAME="vanilla-backup_$(date +%F_%H-%M)_weekly.tar.gz"
             cd /home/jfs/vanilla/
             tar czf /home/jfs/vanilla/backup/$FILENAME world
-            tmux -q send -t 0 "save-on" C-m
+            tmux -q send -t vanilla "save-on" C-m
             cd /home/jfs/vanilla/backup
             /bin/ls -t | grep "weekly" | awk 'NR>10' | xargs rm
             #lftp -c "open -u ********,******** dedibackup-dc2.online.net; mirror -Re . /jfs"
         else
-            tmux -q send -t 0 "save-all" C-m
+            tmux -q send -t vanilla "save-all" C-m
             sleep 1
-            tmux -q send -t 0 "save-off" C-m
+            tmux -q send -t vanilla "save-off" C-m
             FILENAME="vanilla-backup_$(date +%F_%H-%M)_daily.tar.gz"
             cd /home/jfs/vanilla/
             tar czf /home/jfs/vanilla/backup/$FILENAME world
-            tmux -q send -t 0 "save-on" C-m
+            tmux -q send -t vanilla "save-on" C-m
             cd /home/jfs/vanilla/backup
             /bin/ls -t | grep "daily" | awk 'NR>10' | xargs rm
             #lftp -c "open -u ********,******** dedibackup-dc2.online.net; mirror -Re . /jfs"
@@ -95,13 +95,13 @@ overviewertime() {
 # activated by swatch when '.tps' is detected in the vanilla log file
 # runs a debug start/stop on the server console and displays the result back in chat
 tps() {
-    tmux -q send -t 0 "debug start" C-m
+    tmux -q send -t vanilla "debug start" C-m
     sleep 1
-    tmux -q send -t 0 "debug stop" C-m
+    tmux -q send -t vanilla "debug stop" C-m
     sleep .1
     ticks=`tac vanilla/logs/latest.log | grep -m1 ticks | sed -r 's/.*(.{10})/\1/' | sed 's/(//' | sed 's/)//' | sed 's/]//'`
     output="say $ticks per second"
-    tmux -q send -t 0 "$output" C-m
+    tmux -q send -t vanilla "$output" C-m
 }
 
 # runs a function depending on your command argument(s)
